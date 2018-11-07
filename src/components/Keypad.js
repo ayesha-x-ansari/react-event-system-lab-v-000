ba26dde74f43b40229c447ee1438a696b73662a3
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-07 17:43:27 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React from 'react';
+=======
+const React = require('react');
+>>>>>>> aa9dcc4e44a3fd3f05a7a8bd2da0ff81657835f0
 
 class Keypad extends React.Component {
   constructor() {
@@ -12,7 +16,10 @@ class Keypad extends React.Component {
   }
 
   render() {
+<<<<<<< HEAD
     debugger
+=======
+>>>>>>> aa9dcc4e44a3fd3f05a7a8bd2da0ff81657835f0
     return (
       <div>
         <input type="password" onKeyUp={this.handleInputPassword}/>
@@ -21,4 +28,12 @@ class Keypad extends React.Component {
   }
 }
 
+<<<<<<< HEAD
 export default Keypad;
+=======
+<<<<<<< HEAD
+export default Keypad;
+=======
+module.exports = Keypad;
+>>>>>>> aa9dcc4e44a3fd3f05a7a8bd2da0ff81657835f0
+>>>>>>> 71cdab1037c99a80a36813df8e10e482f6cda79c
